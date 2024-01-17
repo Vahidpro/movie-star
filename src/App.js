b7143ce0eb694691ed843406096b7175db0fbe17
Vahidpro <ivahidp@gmail.com>
2024-01-17 13:45:38 +0330

    Added custom hook for fetch movies

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import "./index.css";
 import { useEffect, useRef, useState } from "react";
 import StarRating from "./StarRating";
+import { useMovies } from "./hooks/useMovies";
 
 const average = (arr) =>
 	arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
-
 const KEY = "469c31b8";
 export default function App() {
 	const [query, setQuery] = useState("");
-	const [movies, setMovies] = useState([]);
-	const [isLoading, setIsLoading] = useState(false);
-	const [error, setError] = useState("");
 	const [selectedId, setSelectedId] = useState(null);
+	const { movies, isLoading, error } = useMovies(query);
 	// const [watched, setWatched] = useState([]);
 	const [watched, setWatched] = useState(() => {
 		const storedValue = localStorage.getItem("watched");
@@ -34,50 +32,6 @@ export default function App() {
 	useEffect(() => {
 		localStorage.setItem("watched", JSON.stringify(watched));
 	}, [watched]);
-
-	useEffect(
-		function () {
-			const controller = new AbortController();
-			async function fetchMovies() {
-				try {
-					setIsLoading(true);
-					setError("");
-					const res = await fetch(
-						`http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
-						{ signal: controller.signal }
-					);
-
-					if (!res.ok) throw new Error("Something went wrong with fetching movies");
-
-					const data = await res.json();
-					if (data.Response === "false") throw new Error("Movie not found");
-					setMovies(data.Search);
-					setError("");
-				} catch (error) {
-					console.error(error.message);
-					if (error.name !== "AbortError") {
-						setError(error.message);
-					}
-				} finally {
-					setIsLoading(false);
-				}
-			}
-
-			if (query.length < 3) {
-				setMovies([]);
-				setError("");
-				return;
-			}
-
-			handleCloseMovie();
-
-			fetchMovies();
-			return function () {
-				controller.abort();
-			};
-		},
-		[query]
-	);
 
 	return (
 		<>
